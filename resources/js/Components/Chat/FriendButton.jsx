f456386691f1dab1fd0bb2fad65f8b4392db0492
MohaSetif <BouroubaMohamed004@gmail.com>
2024-04-16 23:42:26 +0100

    Building Send/Receive logic/UI

--- resources/js/Components/Chat/FriendButton.jsx
+++ resources/js/Components/Chat/FriendButton.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 function FriendButton({ friend }) {
-  return (
-    <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-      <div className="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">
-        {friend.picture}
-      </div>
-      <div className="ml-2 text-sm font-semibold">{friend.name}</div>
-    </button>
-  );
+    return (
+        <button className="flex flex-row items-start justify-start hover:bg-gray-100 rounded-xl p-2">
+            <div className="flex items-center justify-center h-9 w-9 bg-indigo-200 rounded-full">
+                U
+            </div>
+            <div className="flex flex-col">
+                <div className="mr-6 text-sm font-semibold">{friend.name}</div>
+                <p className="ml-2 text-xs text-black">
+                    {friend.message}
+                </p>
+            </div>
+        </button>
+    );
 }
 
 export default FriendButton;
--- resources/js/Pages/Chat/chatIndex.jsx
+++ resources/js/Pages/Chat/chatIndex.jsx
@@ -7,24 +7,23 @@ import { useState, useEffect } from 'react';
 export default function chatIndex(props) {
     const { auth, errors, recentMessages, receiver, messages } = props;
     console.log(props);
+
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-        >
+        <AuthenticatedLayout user={auth.user}>
             <Head title="Message Section" />
             <div className="py-2">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="flex h-screen antialiased text-gray-800">
                         <div className="flex flex-row h-full w-full overflow-x-hidden">
-
-                            {/* Users part */}
-
-                            <ChatSidebar/>
-
-                            {/* messages part */}
-
-                            <ChatBox />
-
+                            <ChatSidebar recentMessages={props.recentMessages} />
+                            <div className="flex-grow flex justify-center items-center">
+                                <p className="text-gray-600 mb-20 text-2xl">
+                                    select the user you want to chat with!
+                                </p>
+                            </div>
+                            {/*
+                            <ChatBox /> 
+                            */}
                         </div>
                     </div>
                 </div>
